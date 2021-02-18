b58e1929445232c9902b9ffb273ad41fb842bac9
Maxel Udson Alves da Conceição <maxellopes32@gmail.com>
2021-02-18 11:42:27 -0200

    update

---
+++ course/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace course {
+    class Program {
+        static void Main(string[] args) {
+            bool completo = false;
+            char genero = 'F';
+            byte n1 = 126;
+            int n2 =   1000;
+            float n5 = 4.5f;
+            double n6 = 4.5;
+            string nome = "Maxel";
+            object obj1 = "Alex";
+            int n4 = int.MinValue;
+            int n3 = int.MaxValue;
+            Console.WriteLine(n1);
+            Console.WriteLine(n2);
+            Console.WriteLine(completo);
+            Console.WriteLine(genero);
+            Console.WriteLine(n5);
+            Console.WriteLine(n6);
+            Console.WriteLine(nome);
+            Console.WriteLine(obj1);
+            Console.WriteLine(n4);
+            Console.WriteLine(n3);
+        }
+    }
+}
---
+++ meuPrimeiroPrograma/Ola-mundo.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace meuPrimeiroPrograma {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Olá, mundo!");
+        }
+    }
+}
