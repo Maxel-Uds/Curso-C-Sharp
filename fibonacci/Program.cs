8250dd46303d8017ccfc7847ece73b00cfb55a04
Maxel Udson Alves da Conceição <maxellopes32@gmail.com>
2021-02-24 00:35:19 -0300

    fibonacci

---
+++ fibonacci/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0, y = 0, z = 1;
+            Console.Write(y);
+            for (int i = 1; i <= 15; i++) {
+                x = y + z;
+                Console.Write($" {x}");
+                z = y;
+                y = x;
+            }
+        }
+    }
+}
