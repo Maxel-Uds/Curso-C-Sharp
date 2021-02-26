5fcd99590e12d627cf7e88eed977def5abd47626
Maxel Udson Alves da Conceição <maxellopes32@gmail.com>
2021-02-26 00:02:35 -0300

    Update Program.cs

--- Repetitiva(for)/fatorial/Program.cs
+++ Repetitiva(for)/fatorial/Program.cs
@@ -6,6 +6,7 @@ namespace fatorial
     {
         static void Main(string[] args)
         {
+            Console.Write("Digite um número para calcular o fatorial: ");
             int x = int.Parse(Console.ReadLine());
             int fatorial = 1;
             for (int i = 1; i <= x; i++) {
