425201423b894e41f7f6979abc6e0eb94f4046bf
Maxel Udson Alves da Conceição <maxellopes32@gmail.com>
2021-03-03 18:21:28 -0300

    Exemplo 1 de Listas add

---
+++ Listas/exemplo1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace exemplo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>(); //lista vazia
+
+            List<string> list2 = new List<string> { "Maria", "Alex"};//lista com valores atribuídos
+        }
+    }
+}
