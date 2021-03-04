ebe8e02ffa17accac517a663f2975cf6628eff7b
Maxel Udson Alves da Conceição <maxellopes32@gmail.com>
2021-03-04 18:58:19 -0300

    Exercício 1 de matrizes feito

---
+++ Matrizes/exercicio1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace exercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite a ordem da matriz: ");
+            int n = int.Parse(Console.ReadLine());
+            int[,] mat = new int[n, n];
+
+            for(int i = 0; i < n; i++) {
+                string[] v = Console.ReadLine().Split(' ');
+                for(int j = 0; j < n; j++) {
+                    mat[i, j] = int.Parse(v[j]);
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Diagonal principal:");
+            for(int i = 0; i < n; i++) {
+                Console.Write(mat[i,i] + " ");
+            }
+
+            Console.WriteLine();
+            int negativos = 0;
+            foreach(int obj in mat) {
+                if(obj < 0) {
+                    negativos++;
+                }
+            }
+            Console.WriteLine("O número de valores negativos é: "+ negativos);
+        }
+    }
+}
