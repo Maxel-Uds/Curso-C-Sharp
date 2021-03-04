c476f322a7754c054299a7f16993778e38b58cfd
Maxel Udson Alves da Conceição <maxellopes32@gmail.com>
2021-03-04 17:41:03 -0300

    Exemplo de matrizes add

---
+++ Matrizes/exemplo1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace exemplo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[,] mat = new double[2,3];
+
+            Console.WriteLine(mat.Length);
+            Console.WriteLine(mat.Rank);
+            Console.WriteLine(mat.GetLength(0));
+        }
+    }
+}
