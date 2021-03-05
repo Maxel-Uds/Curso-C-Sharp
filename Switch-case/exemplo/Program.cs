cad1eaeaf372b2e90579b8f57fe40e4ced747353
Maxel Udson Alves da Conceição <maxellopes32@gmail.com>
2021-03-05 17:02:04 -0300

    Exemplo de switch-case add

---
+++ Switch-case/exemplo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace exemplo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Exemplo de escolha caso
+            int x = int.Parse(Console.ReadLine());
+            string dia;
+
+            switch(x) {
+                case 1:
+                    dia = "Domingo";
+                    break;
+                case 2:
+                    dia = "Segunda";
+                    break;
+                case 3:
+                    dia = "Terça";
+                    break;
+                case 4:
+                    dia = "Quarta";
+                    break;
+                case 5:
+                    dia = "Quinta";
+                    break;
+                case 6:
+                    dia = "Sexta";
+                    break;
+                case 7:
+                    dia = "Sábado";
+                    break;
+                default:
+                    dia = "valor inválido";
+                    break;
+            }
+            Console.WriteLine("Hoje é " + dia);
+        }
+    }
+}
