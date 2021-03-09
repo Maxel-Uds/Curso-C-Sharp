bd5e8c70c08d1cbe4c3afcb6e73330f49e1cbea7
Maxel Udson Alves da Conceição <maxellopes32@gmail.com>
2021-03-09 15:21:59 -0300

    Exemplo de TimeSpan e propriedades incompletas de DateTime add

---
+++ DateTime/propriedades/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace propriedades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime d1 = new DateTime(2001, 8, 15, 1);
+
+            Console.WriteLine(d1);
+        }
+    }
+}
---
+++ TimeSpan/exemplo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace exemplo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TimeSpan t1 = new TimeSpan();
+            TimeSpan t2 = new TimeSpan(900000000L);
+            TimeSpan t3 = new TimeSpan(0, 1, 30);
+            TimeSpan t4 = new TimeSpan(1, 2, 11, 21);
+            TimeSpan t5 = TimeSpan.FromDays(1.5);
+
+            Console.WriteLine(t1);
+            Console.WriteLine(t2);
+            Console.WriteLine(t3);
+            Console.WriteLine(t4);
+            Console.WriteLine(t5);
+        }
+    }
+}
