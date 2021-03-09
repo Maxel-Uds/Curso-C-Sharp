2263ba490d88a7bf7385eff26b3cee3999108997
Maxel Udson Alves da Conceição <maxellopes32@gmail.com>
2021-03-09 16:04:21 -0300

    Operações com DateTime add

---
+++ DateTime/operacoes/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace operacoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime d1 = new DateTime(2001, 8, 15, 13, 45, 58);
+            DateTime d5 = new DateTime(2021, 4, 10);
+            DateTime d6 = new DateTime(2000, 4, 10);
+
+            DateTime d2 = d1.AddHours(2);
+            DateTime d3 = d1.AddMinutes(3);
+            DateTime d4 = d1.AddDays(7);
+            TimeSpan t = d5.Subtract(d6);
+
+            Console.WriteLine(d1);
+            Console.WriteLine(d2);
+            Console.WriteLine(d3);
+            Console.WriteLine(d4);
+            Console.WriteLine(t);
+        }
+    }
+}
