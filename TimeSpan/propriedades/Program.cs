32348a67d56cf92e671a53232bc9e0e361c864e5
Maxel Udson Alves da Conceição <maxellopes32@gmail.com>
2021-03-09 16:19:47 -0300

    Propriedades de TimeSpan add

---
+++ TimeSpan/propriedades/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace propriedades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TimeSpan t = new TimeSpan(2, 3, 5, 7, 11);
+
+            Console.WriteLine(t.Days);
+            Console.WriteLine(t.TotalDays);
+        }
+    }
+}
