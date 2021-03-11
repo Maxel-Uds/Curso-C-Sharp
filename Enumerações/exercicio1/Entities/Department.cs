e46de589c134f9f76c9fd18e12c41c9945b4e6dd
Maxel Udson Alves da Conceição <maxellopes32@gmail.com>
2021-03-11 18:58:12 -0300

    Entities add ao exercício1 de enumerações

---
+++ Enumerações/exercicio1/Entities/Department.cs
@@ -0,0 +1,7 @@
+namespace exercicio1.Entities
+{
+    public class Department
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ Enumerações/exercicio1/Entities/HourContract.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace exercicio1.Entities
+{
+    public class HourContract
+    {
+        public DateTime Date { get; set; }
+        public double ValuePerHour { get; set; }
+        public int Hours { get; set; }
+    }
+}
