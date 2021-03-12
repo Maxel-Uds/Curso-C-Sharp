77090f3e1cc954707da8fdfda30195bfbf77a95e
Maxel Udson Alves da Conceição <maxellopes32@gmail.com>
2021-03-12 18:19:02 -0300

    Exercício 2 de enumerações incompleto add

---
+++ Enumerações/exercicio2/Entities/Post.cs
@@ -0,0 +1,12 @@
+using System;
+namespace exercicio2.Entities
+{
+    public class Post
+    {
+        public DateTime Moment { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int Likes { get; set; }
+        public List<Comment> Comments { get; set; } = new List<Comment>();
+    }
+}
---
+++ Enumerações/exercicio2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using exercicio2.Entities;
+
+namespace exercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
