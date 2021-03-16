84c6213f93b72b3eb40acec50f7a2fcbe5d3ed9d
Maxel Udson Alves da Conceição <maxellopes32@gmail.com>
2021-03-16 15:15:32 -0300

    Exemplo 1 de herança add

---
+++ Herança_Polimorfismo/exemplo1/Entities/BusinessAccount.cs
@@ -0,0 +1,24 @@
+namespace exemplo1.Entities
+{
+    public class BusinessAccount : Account
+    {
+        public double LoanLimit { get; set; }
+
+        public BusinessAccount()
+        {
+        }
+
+        public Account(int number, string holder, double balance, double loanLimit) : base(number, holder, balance) //Reutilizando o construtor do Account
+        {
+            LoanLimit = loanLimit;
+        }
+
+        public void Loan(double amount)
+        {
+            if(amount <= LoanLimit)
+            {
+                Balance += amount;
+            }
+        }
+    }
+}
---
+++ Herança_Polimorfismo/exemplo1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace exemplo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
