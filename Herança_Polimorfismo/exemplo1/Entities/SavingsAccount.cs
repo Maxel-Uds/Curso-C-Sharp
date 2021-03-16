8af08e3e45705b1727b386db0633e808bd9fb2e8
Maxel Udson Alves da Conceição <maxellopes32@gmail.com>
2021-03-16 16:18:57 -0300

    Entidade add ao exemplo 1

---
+++ Herança_Polimorfismo/exemplo1/Entities/SavingsAccount.cs
@@ -0,0 +1,21 @@
+namespace exemplo1.Entities
+{
+    public class SavingsAccount : Account
+    {
+        public double InterestRate { get; set; }
+
+        public SavingsAccount()
+        {
+        }
+
+        public SavingsAccount(int number, string holder, double balance, double interestRate) : base(number, holder, balance)
+        {
+            InterestRate = interestRate;
+        }
+
+        public void UpdateBalance()
+        {
+            Balance += Balance * InterestRate; 
+        }
+    }
+}
