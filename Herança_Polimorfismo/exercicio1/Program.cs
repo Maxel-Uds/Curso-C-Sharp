ddc537d07d39c45c3745323f971a2fd4c419b3a4
Maxel Udson Alves da Conceição <maxellopes32@gmail.com>
2021-03-17 15:43:20 -0300

    Exercício 1 de herança/polimorfismo
    Parte 2 add e terminada

--- Herança_Polimorfismo/exercicio1/Program.cs
+++ Herança_Polimorfismo/exercicio1/Program.cs
@@ -14,7 +14,33 @@ namespace exercicio1
 
             for(int i = 1; i <= n; i++)
             {
+                Console.WriteLine($"Dados do funcionário #{i}:");
+                Console.Write("Terceirizado (s/n)? ");
+                char resp = char.Parse(Console.ReadLine());
 
+                Console.Write("Nome: ");
+                string name = Console.ReadLine();
+                Console.Write("Horas: ");
+                int hours = int.Parse(Console.ReadLine());
+                Console.Write("Valor por hora: ");
+                double valuePerHour = double.Parse(Console.ReadLine());
+                if(resp == 's')
+                {
+                    Console.Write("Adicional: ");
+                    double additionalCharge = double.Parse(Console.ReadLine());
+                    employee.Add(new OutSourcedEmployee(name, hours, valuePerHour, additionalCharge)); //polimorfismo/upcasting de lista
+                }
+                else
+                {
+                    employee.Add(new Employee(name, hours, valuePerHour));
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("PAGAMENTOS:");
+            foreach(Employee obj in employee)
+            {
+                Console.WriteLine(obj.Name + " - $" + obj.Payment().ToString("F2"));
             }
         }
     }
