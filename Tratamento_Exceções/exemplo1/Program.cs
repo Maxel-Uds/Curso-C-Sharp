49586f8446abc542be8de1f1170d397eb846d47d
Maxel Udson Alves da Conceição <maxellopes32@gmail.com>
2021-03-18 17:27:42 -0300

    Exemplo 1 de tratamento de exceções add
    Usando Try e Catch

---
+++ Tratamento_Exceções/exemplo1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace exemplo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try {
+                int n1 = int.Parse(Console.ReadLine());
+                int n2 = int.Parse(Console.ReadLine());
+
+                int result = n1 / n2;
+                Console.WriteLine(result);
+            }
+            catch(DivideByZeroException e) {
+                Console.WriteLine("Erro! " + e.Message);
+            }
+            catch(FormatException) {
+                Console.WriteLine("Dado inválido!");
+            }
+        }
+    }
+}
