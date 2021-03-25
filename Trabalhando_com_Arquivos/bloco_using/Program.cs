2c9e95e6e7bd1c40525cd152e498cb6d8a7b9a66
Maxel Udson Alves da Conceição <maxellopes32@gmail.com>
2021-03-25 16:03:06 -0300

    Exemplo com bloco using add

---
+++ Trabalhando_com_Arquivos/bloco_using/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace bloco_using
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\Adyel\Documents\Repositórios\Curso-C-Sharp\Trabalhando_com_Arquivos\bloco_using\file1.txt";
+
+            using(FileStream fs = new FileStream(path, FileMode.Open))
+            {
+                using(StreamReader sr = new StreamReader(fs))
+                {
+                    while(!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+        }
+    }
+}
