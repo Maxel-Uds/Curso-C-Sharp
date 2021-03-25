91f8931527a707ab59c9c2d205c63ba5161bd29b
Maxel Udson Alves da Conceição <maxellopes32@gmail.com>
2021-03-25 17:46:41 -0300

    Exercício com manipulação de arquivos add
    Usando StreamWrite, Path, File e Directory

---
+++ Trabalhando_com_Arquivos/exercicio/Entities/Product.cs
@@ -0,0 +1,21 @@
+namespace exercicio.Entities
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+
+        public Product(string name, double price, int quantity)
+        {
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+        }
+
+        public double TotalValue()
+        {
+            return Price * Quantity;
+        }
+    }
+}
---
+++ Trabalhando_com_Arquivos/exercicio/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Globalization;
+using exercicio.Entities;
+
+namespace exercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"C:\Users\Adyel\Documents\Repositórios\Curso-C-Sharp\Trabalhando_com_Arquivos\exercicio\n.csv";
+            string[] lines = File.ReadAllLines(sourcePath);
+
+            string sourceDirPath = Path.GetDirectoryName(sourcePath);
+            string targetDirPath = sourceDirPath + @"\out";
+            string targetFilePath = targetDirPath + @"\summary.csv";
+
+            Directory.CreateDirectory(targetDirPath);
+
+            using(StreamWriter sw = File.AppendText(targetFilePath))
+            {
+                foreach(string line in lines)
+                {
+                    string[] field = line.Split(',');
+                    string name = field[0];
+                    double price = double.Parse(field[1], CultureInfo.InvariantCulture);
+                    int quantity = int.Parse(field[2]);
+
+                    Product prod = new Product(name, price, quantity);
+                    sw.WriteLine(prod.Name + " | " + prod.TotalValue().ToString("F2", CultureInfo.InvariantCulture));
+                }
+            }
+
+        }
+    }
+}
