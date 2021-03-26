a740040fb45015c9c8d60acdc348e41dc8561879
Maxel Udson Alves da Conceição <maxellopes32@gmail.com>
2021-03-26 15:55:27 -0300

    Exercício 2 com manipulação de arquivos add

---
+++ Trabalhando_com_Arquivos/exercicio2/Entities/Product.cs
@@ -0,0 +1,21 @@
+namespace exercicio2.Entities
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+
+        public Product(string name, double price, int quantity)
+        {
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+        }
+
+        public double TotalValue()
+        {
+            return Price * Quantity;
+        }
+    }
+}
---
+++ Trabalhando_com_Arquivos/exercicio2/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+using System.Globalization;
+using exercicio2.Entities;
+
+namespace exercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Digite o caminho até o arquivo onde ficará guardado os dados da ordem: ");
+                string filePath = @"" + Console.ReadLine();
+                Console.Write("Quantas linhas terá o arquivo " + Path.GetFileName(filePath) + ": ");
+                int n = int.Parse(Console.ReadLine());
+
+                FileStream fs;
+                StreamWriter origin;
+                StreamWriter target;
+                using( fs = new FileStream(filePath, FileMode.Open))
+                {
+                    using(origin = new StreamWriter(fs))
+                    {
+                        for(int i = 1; i <= n; i++)
+                        {
+                            Console.WriteLine($"Digite os dados do produto #{i} (nome,preço,quantidade):");
+                            origin.WriteLine(Console.ReadLine());
+                        }
+                    }
+                }
+                string[] lines = File.ReadAllLines(filePath);//Usa-se sem instanciar
+
+                Console.Write("Digite o caminho até o arquivo onde ficará guardado o resumo da ordem: ");
+                string targetPath = @"" + Console.ReadLine(); 
+
+                using(fs = new FileStream(targetPath, FileMode.Open))
+                {
+                    using(target = new StreamWriter(fs))
+                    {
+                        foreach(string line in lines)
+                        {
+                            string[] field = line.Split(',');
+                            string name = field[0];
+                            double price = double.Parse(field[1], CultureInfo.InvariantCulture);
+                            int quantity = int.Parse(field[2]);
+
+                            Product prod = new Product(name, price, quantity);
+
+                            target.WriteLine(prod.Name.ToUpper() + ", " + prod.TotalValue().ToString("F2", CultureInfo.InvariantCulture));
+                        }
+                    }
+                }
+
+                Console.Clear();
+                lines = File.ReadAllLines(filePath);
+                foreach(string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+                Console.WriteLine();
+                lines = File.ReadAllLines(targetPath);
+                foreach(string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
