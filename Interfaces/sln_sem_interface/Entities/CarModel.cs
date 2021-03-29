35a6c3a15db67f14c8fd42ddd075a37abbf75839
Maxel Udson Alves da Conceição <maxellopes32@gmail.com>
2021-03-29 15:32:27 -0300

    Minha versão da solução sem interface

---
+++ Interfaces/sln_sem_interface/Entities/CarModel.cs
@@ -0,0 +1,12 @@
+namespace sln_sem_interface.Entities
+{
+    public class CarModel
+    {
+        public string Name { get; set; }
+
+        public CarModel(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ Interfaces/sln_sem_interface/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using sln_sem_interface.Entities;
+
+namespace sln_sem_interface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com os dados da locação:");
+            Console.Write("Modelo do carro: ");
+            string name = Console.ReadLine();
+            Console.Write("Retirada (DD/MM/AAAA HH:MM): ");
+            DateTime pickUp = DateTime.Parse(Console.ReadLine());
+            Console.Write("Entrega (DD/MM/AAAA HH:MM): ");
+            DateTime retur = DateTime.Parse(Console.ReadLine());
+            Console.Write("Valor por hora: ");
+            double valuePerHour = double.Parse(Console.ReadLine());
+            Console.Write("Valor por dia: ");
+            double valuePerDay = double.Parse(Console.ReadLine());
+
+            CarModel model = new CarModel(name);
+            Location location = new Location(model, pickUp, retur, valuePerHour, valuePerDay);
+
+            Console.WriteLine();
+            Console.WriteLine("RECIBO:");
+            Console.WriteLine("Pagamento básico: " + location.Payment().ToString("F2"));
+            Console.WriteLine("Taxa: " + location.Tax().ToString("F2"));
+            Console.WriteLine("Total: " + location.TotalPayment().ToString("F2"));
+
+        }
+    }
+}
