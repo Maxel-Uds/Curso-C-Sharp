88b482732452cfd2940e633edbfbf4483c1a688f
Maxel Udson Alves da Conceição <maxellopes32@gmail.com>
2021-04-01 15:21:24 -0300

    Exemplo 1 de conjuntos usando HashSet

---
+++ Generics_Set_Dictionary/Conjuntos/exemplo1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace exemplo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            HashSet<string> set = new HashSet<string>();
+            
+            set.Add("Tv");
+            set.Add("Notebook");
+            set.Add("Tablet");
+
+            Console.WriteLine(set.Contains("Notebook"));
+            foreach(string obj in set)
+            {
+                Console.WriteLine(obj);
+            }
+        }
+    }
+}
