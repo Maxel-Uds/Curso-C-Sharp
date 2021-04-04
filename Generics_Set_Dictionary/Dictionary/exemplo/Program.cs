1c04a0d96c0644b759a48d6d4fb13d404d40dbe0
Maxel Udson Alves da Conceição <maxellopes32@gmail.com>
2021-04-04 11:15:52 -0300

    Exemplo com uso do Dictionary add

---
+++ Generics_Set_Dictionary/Dictionary/exemplo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace exemplo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> cookies = new Dictionary<string, string>();
+
+            cookies["user"] = "maria";
+            cookies["email"] = "maria@gmail.com";
+            cookies["phone"] = "99712234";
+
+            Console.WriteLine(cookies["phone"]);
+            Console.WriteLine(cookies["email"]);
+
+            cookies.Remove("email");
+
+            if(cookies.ContainsKey("email"))
+            {
+                Console.WriteLine(cookies["email"]);
+            }
+            else
+            {
+                Console.WriteLine("Email não encontrado!");
+            }
+
+            Console.WriteLine("Tamanho: " + cookies.Count);
+
+            foreach(KeyValuePair<string, string> obj in cookies)
+            {
+                Console.WriteLine(obj.Key + ": " + obj.Value);
+            }
+        }
+    }
+}
