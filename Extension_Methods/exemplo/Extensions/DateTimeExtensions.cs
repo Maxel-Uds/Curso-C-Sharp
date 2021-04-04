8de8fc40e0031676e936d66556e28f5a8287e4e6
Maxel Udson Alves da Conceição <maxellopes32@gmail.com>
2021-04-04 13:46:15 -0300

    Exemplo usando métodos de extensão add

---
+++ Extension_Methods/exemplo/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace System
+{
+    public static class DateTimeExtensions
+    {
+        public static string ElapsedTime(this DateTime thisObj)
+        {
+            TimeSpan duration = DateTime.Now.Subtract(thisObj);
+
+            if(duration.TotalHours < 24)
+            {
+                return duration.TotalHours.ToString("F1") + " horas";
+            }
+            else
+            {
+                return duration.TotalDays.ToString("F1") + " dias";
+            }
+        }
+    }
+}
---
+++ Extension_Methods/exemplo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace exemplo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime(2018, 11, 16, 8, 10, 45);
+            Console.WriteLine(dt.ElapsedTime());
+        }
+    }
+}
