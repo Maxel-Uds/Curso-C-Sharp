d99441497e32a10392e7bb569b00ea9f821e35b2
Maxel Udson Alves da Conceição <maxellopes32@gmail.com>
2021-04-08 12:09:20 -0300

    Exemplo 2 usando multicast delegates

---
+++ Lambda_Delegates_LINQ/Delegates/exemplo2/Services/CalculationService.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace exemplo2.Services
+{
+    public class CalculationService
+    {
+        public static void ShowMax(double x, double y)
+        {
+            double max = (x > y)? x:y;
+            Console.WriteLine(max);
+        }
+
+        public static void ShowSum(double x, double y)
+        {
+            double sum = x + y;
+            Console.WriteLine(sum);
+        }
+    }
+}
