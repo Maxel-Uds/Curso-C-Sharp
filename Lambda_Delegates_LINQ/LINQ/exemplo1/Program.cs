366b97f344d60b460efc0949f3e6f6389982e77c
Maxel Udson Alves da Conceição <maxellopes32@gmail.com>
2021-04-08 14:43:26 -0300

    Exemplo 1 de LINQ usando os 3 passos

---
+++ Lambda_Delegates_LINQ/LINQ/exemplo1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace exemplo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Specify data source
+            int[] numbers = new int[] {2, 3 ,4 ,5};
+
+            //Define the query expression
+            IEnumerable<int> result = numbers.Where(x => x % 2 == 0).Select(x => x * 10);
+
+            //Execute query
+            foreach(int x in result)
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
