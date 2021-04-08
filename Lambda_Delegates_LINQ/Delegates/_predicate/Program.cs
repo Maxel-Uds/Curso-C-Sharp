ab0e62ca76424ad964fc48ef5a533a07da3f8fb4
Maxel Udson Alves da Conceição <maxellopes32@gmail.com>
2021-04-08 12:35:50 -0300

    Demo de predicate add

---
+++ Lambda_Delegates_LINQ/Delegates/_predicate/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using _predicate.Entities;
+using System.Collections.Generic;
+
+namespace _predicate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+
+            list.Add(new Product("Tv", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.50));
+            list.Add(new Product("HD Case", 80.90));
+
+            list.RemoveAll(ProductTest);
+
+            foreach(Product item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public static bool ProductTest(Product p)
+        {
+            return p.Price >= 100.0;
+        }
+    }
+}
