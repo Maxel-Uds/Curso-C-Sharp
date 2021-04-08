25445ced9d1b1531b2f2216ee7fe0535e2cfa0ac
Maxel Udson Alves da Conceição <maxellopes32@gmail.com>
2021-04-08 11:58:05 -0300

    Exemplo 1 de delegates add

---
+++ Lambda_Delegates_LINQ/Delegates/exemplo1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using exemplo1.Services;
+
+namespace exemplo1
+{
+    delegate double BinaryNumericOperation(double n1, double n2);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+
+            BinaryNumericOperation op = CalculationService.Sum;
+
+            double result = op(a, b);
+
+            Console.WriteLine(result);
+        }
+    }
+}
